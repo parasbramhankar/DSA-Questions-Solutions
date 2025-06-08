13e433f0f2ab5f9c9f6538f64b1e562dabaed417
parasbramhankar <parasbramhankar02@gmail.com>
2025-06-08 17:30:42 +0530

    check Prime Commit

---
+++ src/Mathematics/CheckPrime/BruteForce.java
@@ -0,0 +1,37 @@
+package Mathematics.CheckPrime;
+
+import java.util.Scanner;
+
+public class BruteForce {
+
+    static boolean checkPrime(int n){
+
+        int c=0;
+
+        for(int i=2;i<=(n-1);i++){
+            if(n%i==0){
+                c=1;
+                break;
+            }
+        }
+
+        if(c==1){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+
+        if(checkPrime(n)){
+            System.out.println(n+" is Prime");
+        }
+        else{
+            System.out.println(n+" is not Prime");
+        }
+
+
+    }
+}
---
+++ src/Mathematics/CheckPrime/Optimize.java
@@ -0,0 +1,42 @@
+package Mathematics.CheckPrime;
+
+import java.util.Scanner;
+
+public class Optimize {
+
+    static boolean checkPrime(int n){
+
+        if(n<=1){
+            return false;
+        }
+
+        if(n==2||n==3){
+            return true;
+        }
+
+        if(n%2==0 || n%3==0){
+            return false;
+        }
+
+        for(int i=5;i<=(int)Math.sqrt(n);i+=5){
+            if(n%i==0 || n%(i+2)==0){
+                return false;
+            }
+        }
+        return  true;
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+
+        if(checkPrime(n)){
+            System.out.println(n+" is Prime");
+        }
+        else{
+            System.out.println(n+" is not Prime");
+        }
+
+
+    }
+}
