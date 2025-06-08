b6f6636263b052f9e7d97a5597127846616784ef
parasbramhankar <parasbramhankar02@gmail.com>
2025-06-08 17:31:02 +0530

    find Prime Commit

---
+++ src/Mathematics/FindAllPrime/Optimize_SieveOfEratosthenesAlgorithm.java
@@ -0,0 +1,43 @@
+package Mathematics.FindAllPrime;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Optimize_SieveOfEratosthenesAlgorithm {
+
+
+    static void printPrime(int n){
+
+        boolean[]prime=new boolean[n+1];
+        Arrays.fill(prime,true);
+
+        for(int i=2;i<=n;i++){
+
+            if(prime[i]){
+
+                for(int j=i*i; j<=n;j+=i){
+                    prime[j]=false;
+                }
+
+            }
+
+        }
+
+        int count=0;
+
+        for(int i=2;i<=n;i++){
+            if(prime[i]){
+                System.out.print(i+" ");
+                count++;
+            }
+        }
+
+        System.out.println("\nTotal number of prime number: "+count);
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+
+        printPrime(n);
+    }
+}
