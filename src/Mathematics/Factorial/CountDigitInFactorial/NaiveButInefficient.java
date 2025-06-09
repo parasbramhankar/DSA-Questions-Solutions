31dafe1c9a00bd2c77df9f7f689c7e172f91375b
parasbramhankar <parasbramhankar02@gmail.com>
2025-06-09 10:53:36 +0530

    Optimization

---
+++ src/Mathematics/Factorial/CountDigitInFactorial/NaiveButInefficient.java
@@ -0,0 +1,26 @@
+package Mathematics.Factorial.CountDigitInFactorial;
+
+public class NaiveButInefficient {
+
+
+    /// This solution doesn't solve a complete problem
+
+/*
+
+🔢What is the Problem?
+
+
+For example:
+
+`5! = 120` → 3 digits
+`10! = 3,628,800` → 7 digits
+
+ But for large numbers like `100!`, the value is enormous:
+100! = 933,262,154,439... (158 digits long!)
+
+So if you try to compute it normally for big numbers,
+the result is **too large for most data types** (even `BigInteger` can be slow).
+
+    */
+
+}
---
+++ src/Mathematics/Factorial/CountDigitInFactorial/Optimize.java
@@ -0,0 +1,26 @@
+package Mathematics.Factorial.CountDigitInFactorial;
+
+import java.util.Scanner;
+
+public class Optimize{
+
+    public static int digitsInFactorial(int N){
+
+        // code here
+        double sum=0;
+
+        for(int i=1;i<=N;i++){
+            sum+=Math.log10(i);
+        }
+
+        sum=Math.floor(sum)+1;
+
+        return (int)(sum);
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        System.out.println("Total Number of digits in "+n+"! : "+digitsInFactorial(n));
+    }
+}
