59c2689a54acdc5862e4d5e119e553daeb309fd7
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-07 09:02:21 +0530

    commit

--- src/Array/Medium/MergeIntervals56/BruteForce.java
+++ src/Array/Medium/MergeIntervals56/BruteForce.java
@@ -49,3 +49,42 @@ public class BruteForce {
         System.out.println(ans);
     }
 }
+
+
+/*
+Returning 2D array
+
+class Solution {
+    public int[][] merge(int[][] arr) {
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        int n = arr.length;
+
+        List<int[]> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int start = arr[i][0];
+            int end = arr[i][1];
+
+            // Skip if current interval is already covered
+            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1)[1]) {
+                continue;
+            }
+
+            // Extend interval by merging overlapping ones
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j][0] <= end) {
+                    end = Math.max(end, arr[j][1]);
+                } else {
+                    break;
+                }
+            }
+
+            ans.add(new int[]{start, end});
+        }
+
+        // Convert List<int[]> to int[][]
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
+
+ */
