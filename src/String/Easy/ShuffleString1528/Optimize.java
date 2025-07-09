72c1f0f07e07169260ba48d188aefd684c3c21f4
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-09 10:44:39 +0530

    Commit

---
+++ src/String/Easy/ShuffleString1528/Optimize.java
@@ -0,0 +1,17 @@
+package String.Easy.ShuffleString1528;
+
+public class Optimize {
+
+
+    public String restoreString(String s, int[] indices) {
+        char[] result = new char[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            result[indices[i]] = s.charAt(i);
+        }
+
+        return new String(result);
+    }
+
+
+}
