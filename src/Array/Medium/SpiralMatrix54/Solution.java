a3a4943a52f7f7271221304f7f56397a10f11cde
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-10 14:00:02 +0530

    Commit

---
+++ src/Array/Medium/SpiralMatrix54/Solution.java
@@ -0,0 +1,51 @@
+package Array.Medium.SpiralMatrix54;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            // Top row
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+
+            // Right column
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                // Bottom row
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                // Left column
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int [][]matrix=new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+
+        List<Integer>list=spiralOrder(matrix);
+
+        System.out.println("Spiral matrix: "+list);
+    }
+}
