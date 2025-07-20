8f905341799f565bb794fabac9ebb7939250602a
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-20 10:23:20 +0530

    Linked List Commit

---
+++ src/LinkedList/SinglyLinkedList/Easy/LinkedListCycle141/OptimizeSolution.java
@@ -0,0 +1,35 @@
+package LinkedList.SinglyLinkedList.Easy.LinkedListCycle141;
+
+public class OptimizeSolution {
+
+
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+     */
+
+        public boolean hasCycle(ListNode head) {
+            ListNode slow=head;
+            ListNode fast=head;
+
+            while(fast!=null && fast.next!=null){
+                slow=slow.next;
+                fast=fast.next.next;
+
+                if(slow==fast){
+                    return true;
+                }
+
+
+            }
+            return false;
+        }
+
+}
