6a82b50a7ce25454f1a4331a8c1d3d740b653c74
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-20 10:23:14 +0530

    Linked List Commit

---
+++ src/LinkedList/SinglyLinkedList/Easy/LinkedListCycle141/ListNode.java
@@ -0,0 +1,19 @@
+package LinkedList.SinglyLinkedList.Easy.LinkedListCycle141;
+
+public class ListNode {
+    int data;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int data) {
+        this.data = data;
+    }
+
+    ListNode(int data, ListNode next) {
+        this.data = data;
+        this.next = next;
+    }
+
+}
