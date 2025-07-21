f9ec9da038a96ab0d0e80324095a830fe1bf12fe
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-21 12:47:29 +0530

    Linked List Commit

---
+++ src/LinkedList/SinglyLinkedList/Easy/PalindromeLinkedList234/ListNode.java
@@ -0,0 +1,19 @@
+package LinkedList.SinglyLinkedList.Easy.PalindromeLinkedList234;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int data) {
+        this.val = data;
+    }
+
+    ListNode(int data, ListNode next) {
+        this.val = data;
+        this.next = next;
+    }
+
+}
