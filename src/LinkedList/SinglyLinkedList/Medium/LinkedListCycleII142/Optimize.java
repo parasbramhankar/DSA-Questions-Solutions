1cff490b6b5dd153037e9441dfb383e2c1e794c5
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-21 12:17:24 +0530

    Linked List Commit

--- src/LinkedList/SinglyLinkedList/Medium/LinkedListCycleII142/Optimize.java
+++ src/LinkedList/SinglyLinkedList/Medium/LinkedListCycleII142/Optimize.java
@@ -1,4 +1,51 @@
 package LinkedList.SinglyLinkedList.Medium.LinkedListCycleII142;
 
 public class Optimize {
+    static public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                break;
+            }
+
+        }
+
+        if (fast== null || fast.next == null) {
+            return null;
+        }
+
+        fast=head;
+        while(fast!=slow){
+            fast=fast.next;
+            slow=slow.next;
+        }
+
+        return slow;
+
+    }
+    public static void main(String[] args) {
+        // Creating nodes
+        ListNode head = new ListNode(3);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(0);
+        ListNode fourth = new ListNode(-4);
+
+        // Linking nodes
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = second; // Creating a cycle back to node with value 2
+
+        ListNode result = detectCycle(head);
+        if (result != null) {
+            System.out.println("Cycle starts at node with value: " + result.data);
+        } else {
+            System.out.println("No cycle detected.");
+        }
+    }
 }
