f4a311319c584619b792ea49715ce2c5781448a9
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-21 12:17:22 +0530

    Linked List Commit

---
+++ src/LinkedList/SinglyLinkedList/Medium/LinkedListCycleII142/ListNode.java
@@ -0,0 +1,12 @@
+package LinkedList.SinglyLinkedList.Medium.LinkedListCycleII142;
+
+public class ListNode {
+    int data;
+    ListNode next;
+
+    ListNode(int data){
+        this.data=data;
+        this.next=null;
+    }
+
+}
