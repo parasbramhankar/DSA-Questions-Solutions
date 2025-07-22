83fabcf55b81da207f3244e813a5526ffe511c15
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-22 11:11:49 +0530

    Linked List Commit

---
+++ src/LinkedList/SinglyLinkedList/Medium/AddTwoNumbers2/ListNode.java
@@ -0,0 +1,12 @@
+package LinkedList.SinglyLinkedList.Medium.AddTwoNumbers2;
+
+public class ListNode {
+    int data;
+    ListNode next;
+
+    ListNode(int data){
+        this.data=data;
+        this.next=null;
+    }
+
+}
