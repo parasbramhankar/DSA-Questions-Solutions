d20c88ed59d5da1eb0320d00a88da0d2f525debc
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-22 09:13:10 +0530

    Linked List Commit

---
+++ src/LinkedList/SinglyLinkedList/Medium/RemoveMiddleNode2095/ListNode.java
@@ -0,0 +1,12 @@
+package LinkedList.SinglyLinkedList.Medium.RemoveMiddleNode2095;
+
+public class ListNode {
+    int data;
+    ListNode next;
+
+    ListNode(int data){
+        this.data=data;
+        this.next=null;
+    }
+
+}
