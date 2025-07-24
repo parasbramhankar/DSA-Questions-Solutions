f86d6f7770e55b08661660adf12b99152a216882
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-24 11:09:28 +0530

    Linked List Commit

---
+++ src/LinkedList/SinglyLinkedList/Medium/OddEvenLinkedList328/ListNode.java
@@ -0,0 +1,11 @@
+package LinkedList.SinglyLinkedList.Medium.OddEvenLinkedList328;
+
+public class ListNode {
+    int data;
+    ListNode next;
+
+    ListNode(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
