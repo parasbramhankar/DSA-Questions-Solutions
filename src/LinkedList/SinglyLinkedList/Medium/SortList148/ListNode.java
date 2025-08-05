ad8980bb2cc1124a915f90650649ade2b8baa8b3
parasbramhankar <parasbramhankar.in@gmail.com>
2025-08-05 09:46:46 +0530

    Linked List Commit

---
+++ src/LinkedList/SinglyLinkedList/Medium/SortList148/ListNode.java
@@ -0,0 +1,18 @@
+package LinkedList.SinglyLinkedList.Medium.SortList148;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
