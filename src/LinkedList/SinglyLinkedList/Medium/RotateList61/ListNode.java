dea5f96450903b2c8c40638c1a8d6d2e3aa12506
parasbramhankar <parasbramhankar.in@gmail.com>
2025-08-08 10:26:20 +0530

    Linked List Commit

---
+++ src/LinkedList/SinglyLinkedList/Medium/RotateList61/ListNode.java
@@ -0,0 +1,12 @@
+package LinkedList.SinglyLinkedList.Medium.RotateList61;
+
+public class ListNode {
+    int data;
+    ListNode next;
+
+    ListNode(int data){
+        this.data=data;
+        this.next=null;
+    }
+
+}
