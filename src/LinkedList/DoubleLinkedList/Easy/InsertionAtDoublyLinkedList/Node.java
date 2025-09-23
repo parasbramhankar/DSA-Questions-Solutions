300351ae50f5ad0dc1c6772a444c7e975e2250c2
parasbramhankar <parasbramhankar.in@gmail.com>
2025-09-23 20:42:27 +0530

    18/09/2025

---
+++ src/LinkedList/DoubleLinkedList/Easy/InsertionAtDoublyLinkedList/Node.java
@@ -0,0 +1,12 @@
+package LinkedList.DoubleLinkedList.Easy.InsertionAtDoublyLinkedList;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        next = prev = null;
+    }
+}
