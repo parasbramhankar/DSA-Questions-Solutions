5cc12907a99662b7c49952c6900c54523accddae
parasbramhankar <parasbramhankar.in@gmail.com>
2025-09-27 12:08:12 +0530

    27/09/2025

---
+++ src/String/Medium/LongestPalindromicSubstring5/Optimize.java
@@ -0,0 +1,35 @@
+package String.Medium.LongestPalindromicSubstring5;
+
+class Optimize {
+    // Expand palindrome around center and return length
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1; // length of palindrome
+    }
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);   // Odd-length palindrome
+            int len2 = expandAroundCenter(s, i, i + 1); // Even-length palindrome
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
