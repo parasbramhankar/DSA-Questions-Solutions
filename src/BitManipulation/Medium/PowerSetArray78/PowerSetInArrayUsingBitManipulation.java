85c28136ea4d2304a01291fe54cefbf444acaea9
parasbramhankar <parasbramhankar.in@gmail.com>
2025-10-21 11:40:05 +0530

    Bit manipulation

--- src/BitManipulation/Medium/PowerSetArray78/PowerSetInArrayUsingBitManipulation.java
+++ src/BitManipulation/Medium/PowerSetArray78/PowerSetInArrayUsingBitManipulation.java
@@ -97,6 +97,7 @@ public class PowerSetInArrayUsingBitManipulation {
     }
     public static void main(String[] args) {
 
+
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter the number of Array elements: ");
         int n=scanner.nextInt();
