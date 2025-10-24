4b473fe6259b1919d0935a02be0abb67c4a8979f
parasbramhankar <parasbramhankar.in@gmail.com>
2025-10-24 19:35:26 +0530

    Linked List

---
+++ src/LinkedList/SinglyLinkedList/Hard/ReverseNodesInkGroup25/ListNode.java
@@ -0,0 +1,23 @@
+package LinkedList.SinglyLinkedList.Hard.ReverseNodesInkGroup25;
+
+public class ListNode {
+
+    // Definition for singly-linked list.
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
