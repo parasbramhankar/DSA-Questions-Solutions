5518ee6c6f3664026b644d03d3c4c98f110e2873
parasbramhankar <parasbramhankar.in@gmail.com>
2025-10-29 05:57:57 +0530

    Recursion

---
+++ src/Recursion/Medium/CheckIfThereExistASubsequenceWithSumK/RecursiveSolution.java
@@ -0,0 +1,23 @@
+package Recursion.Medium.CheckIfThereExistASubsequenceWithSumK;
+
+public class RecursiveSolution {
+    static boolean helper(int[] arr, int k, int ind, int sum) {
+        // If the sum equals target, and we've picked at least one element
+        if(sum == k && ind > 0) {
+            return true;
+        }
+        // If we've gone through all elements
+        if (ind == arr.length) {
+            return false;
+        }
+        // Include current element
+        if(helper(arr, k, ind+1, sum + arr[ind])) {
+            return true;
+        }
+        // Exclude current element
+        if(helper(arr, k, ind+1, sum)) {
+            return true;
+        }
+        return false;
+    }
+}
