3d02c8beae23535adf228aa65f9e5de4bce6ac57
parasbramhankar <parasbramhanakar.in@gmail.com>
2025-11-10 22:53:17 +0530

    Commit

--- src/Stack/Easy/MinStack155/MinStack.java
+++ src/Stack/Easy/MinStack155/MinStack.java
@@ -3,7 +3,6 @@ package Stack.Easy.MinStack155;
 import java.util.Stack;
 
 class MinStack {
-
     /*
      * Problem: LeetCode 155 — Min Stack
      * ----------------------------------
@@ -157,11 +156,3 @@ class MinStack {
     }
 }
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
