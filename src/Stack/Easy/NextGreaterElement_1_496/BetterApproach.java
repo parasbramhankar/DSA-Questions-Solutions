0ab211b1755a8c8c3e083dcb56df9c3c205939f4
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-12 06:18:12 +0530

    Commit

---
+++ src/Stack/Easy/NextGreaterElement_1_496/BetterApproach.java
@@ -0,0 +1,27 @@
+package Stack.Easy.NextGreaterElement_1_496;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class BetterApproach {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack=new Stack<>();
+        HashMap<Integer,Integer> hs=new HashMap<>();
+
+        for(int i=0;i<nums2.length;i++){
+
+            while(!stack.isEmpty() && stack.peek()<=nums2[i]){
+                hs.put(stack.peek(), nums2[i]);
+                stack.pop();
+            }
+            stack.push(nums2[i]);
+        }
+        int []ans=new int[nums1.length];
+
+        for(int i=0;i<nums1.length;i++){
+            ans[i] = hs.getOrDefault(nums1[i], -1);
+        }
+
+        return ans;
+    }
+}
